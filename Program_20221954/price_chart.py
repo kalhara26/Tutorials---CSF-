e8a84b4441a3b1552411c308826357da495fb9c0
Kalhara Thabrew <127278129+kalhara26@users.noreply.github.com>
2023-11-13 23:46:25 +0530

    Delete Program_20221954 directory

--- Program_20221954/price_chart.py
+++
@@ -1,7 +0,0 @@
-price_chart = {
-    "Alvin": {"Alvin": 0, "Jamz": 20, "Razi": 40, "Mali": 40, "Zuhar": 20},
-    "Jamz": {"Alvin": 20, "Jamz": 0, "Razi": 20, "Mali": 40, "Zuhar": 40},
-    "Razi": {"Alvin": 40, "Jamz": 20, "Razi": 0, "Mali": 20, "Zuhar": 40},
-    "Mali": {"Alvin": 40, "Jamz": 40, "Razi": 20, "Mali": 0, "Zuhar": 20},
-    "Zuhar": {"Alvin": 20, "Jamz": 40, "Razi": 40, "Mali": 20, "Zuhar": 0}
-}
--- Program_20221954/vehicle_prices.py
+++
@@ -1,5 +0,0 @@
-vehicle_prices = {
-    "Trishaw": 1,
-    "Car": 2,
-    "Van": 3
-}
